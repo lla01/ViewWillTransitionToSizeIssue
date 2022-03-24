d533e98f8b9a3246bcbb72c021c15bc6a5d4f62c
Lutz Lameyer <lutz.lameyer@chefkoch.de>
2022-03-24 15:35:05 +0100

    Initial Commit

---
+++ ViewWillTransitionToSizeIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewWillTransitionToSizeIssue
+//
+//  Created by Lutz Lameyer on 24.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
